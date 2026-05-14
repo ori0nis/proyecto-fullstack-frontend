a59f94bed4444e6c04256d7cf3e9c39b09502b32
ori0nis <oihanegdcl@gmail.com>
2026-05-14 22:00:13 +0200

    fix: type error in nursery plant add

--- src/utils/axiosErrorHandler.util.ts
+++ src/utils/axiosErrorHandler.util.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosErrorHandler = (error: unknown): Error => {
   if (axios.isAxiosError(error) && error.response) {
-    const backendError = error.response.data?.message || "Unexpected error"
+    const backendError = error.response.data?.message || "Unexpected error";
     return new Error(backendError);
   } else {
     return new Error("Network error or unexpected issue");
